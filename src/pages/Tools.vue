2e5e3b378483993a966e936ba9cdb15c2d1b6c41
飞蛛 <669483446@qq.com>
2022-12-07 19:56:41 +0800

    【大改】删除lifeline相关的组件和页面

--- src/pages/Tools.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <Particles />
-  <Header page="tools"/>
-  <a-row style="margin-top: 22%" align="center" justify="center">1</a-row>
-</template>
-<script setup lang="ts">
-import Particles from "@/components/Particles/index.vue";
-import Header from "@/components/Header.vue";
-</script>
