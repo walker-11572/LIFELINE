d0c76fa81c2954ceff07b4f7d45bebffa39f9865
飞蛛 <669483446@qq.com>
2022-12-11 14:55:43 +0800

    【新增】首页-topiclist-navTab

--- src/pages/Community/Home.vue
+++ src/pages/Community/Home.vue
@@ -29,23 +29,38 @@
     </a-col>
   </a-row>
   <!-- 更新提示 -->
-  <a-row>
+  <!-- <a-row>
     <a-alert banner style="padding-left: 60px">
       <a-col :offset="1" style="padding-left: 1.2vw"
         >4 new posts are added recently, click here to load them.</a-col
       >
     </a-alert>
+  </a-row> -->
+  <a-row>
+    <!-- 帖子 -->
+    <a-col :span="18" class="mt-4 list-nav">
+      <!-- 帖子列表头 -->
+      <a-tabs default-active-key="1">
+        <a-tab-pane key="1" title="推荐" class="p-1">
+          <PostCard
+            :Title="store.topic.title"
+            :Category="store.topic.category"
+            :ExtraInfos="ExtraInfos"
+          />
+        </a-tab-pane>
+        <a-tab-pane key="2" title="最新"> Content of Tab Panel 2 </a-tab-pane>
+        <a-tab-pane key="3" title="热门"> Content of Tab Panel 3 </a-tab-pane>
+      </a-tabs>
+    </a-col>
+    <!-- 侧边栏 -->
+    <a-col :span="6">
+      <a-card class="ms-4 mt-4" :bordered="false"></a-card>
+    </a-col>
   </a-row>
-  <!-- 帖子 -->
-  <PostCard
-    :Title="store.topic.title"
-    :Category="store.topic.category"
-    :ExtraInfos="ExtraInfos"
-  />
 </template>
 <script setup lang="ts">
 import PostCard from "@/components/Community/PostCard.vue";
-import { reactive } from "vue";
+import { reactive, onMounted } from "vue";
 import { mainStore } from "@/store/index";
 const store = mainStore();
 const images = ["/1.jpg", "/2.png", "/3.jpg", "/4.jpg", "/5.jpg"];
@@ -55,5 +70,13 @@ const ExtraInfos = reactive({
   Views: store.topic.views,
   Activity: "1h",
 });
+onMounted(() => {
+  document.querySelector(".arco-tabs-content").style.padding = "0";
+  document.querySelector(".arco-tabs-nav-tab-list").style.marginLeft = "18px";
+});
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.list-nav {
+  background-color: var(--color-bg-2);
+}
+</style>
