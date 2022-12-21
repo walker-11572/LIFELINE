71730cd0df863146c032c973afcbcef661a06c45
飞蛛 <669483446@qq.com>
2022-12-21 11:42:56 +0800

    【小改】页面布局容器

--- src/pages/Community/Community.vue
+++ src/pages/Community/Community.vue
@@ -1,16 +1,14 @@
 <template>
   <Particles />
   <CHeader />
-  <a-row align="center">
-    <a-col
-      :lg="{ span: 18, offset: 3 }"
-      :xxl="{ span: 16, offset: 4 }"
-      style="top: 60px"
-      class="position-absolute pe-xxl-3"
-    >
-      <router-view />
-    </a-col>
-  </a-row>
+  <a-col
+    :lg="{ span: 18 }"
+    :xxl="{ span: 16 }"
+    style=" margin: 0 auto;"
+    class="pe-xxl-3"
+  >
+    <router-view />
+  </a-col>
 </template>
 <script setup lang="ts">
 import Particles from "@/components/Particles/index.vue";
